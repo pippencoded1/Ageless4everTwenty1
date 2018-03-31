4de2b81ce5461c2702da7c87bbbefe33739c2ace
Ali Huda <alihuda1992@gmail.com>
2018-03-31 17:15:51 -0400

    updating

--- pillsalad.js
+++ pillsalad.js
@@ -1,22 +1,14 @@
-
-
-
-
-
-
-
-
-
-
-
  $(document).ready(function () {
 
-        var term = $("#usertext").val().trim();
+       
 
         $("#submit").click(function () {
-        var xhr = $.getJSON("https://api.urbandictionary.com/v0/define?term=" + term);
+        $("#urbantext").html("");
+        var term = $(".form-control").val().trim();
+        var xhr = $.get("https://api.urbandictionary.com/v0/define?term="+term);
         xhr.done(function (response) {
             console.log(response)
+        $("#urbantext").append(response.list[0].definition);
            
     //  close response function
         });
