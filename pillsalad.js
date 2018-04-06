1ca842012edd3691e10b855ac1146f25e2049a0a
Unknown <madinah2491@gmail.com>
2018-04-05 21:06:21 -0400

    added firebase + edited css

--- pillsalad.js
+++ pillsalad.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+  
+   // Initialize Firebase
+   var config = {
+    apiKey: "AIzaSyCo4DJ1MiPYKNmGVt1HdF3mCE1mqAm-BZg",
+    authDomain: "slangtube-recentsearch.firebaseapp.com",
+    databaseURL: "https://slangtube-recentsearch.firebaseio.com",
+    projectId: "slangtube-recentsearch",
+    storageBucket: "slangtube-recentsearch.appspot.com",
+    messagingSenderId: "1084533016118"
+  };
+  firebase.initializeApp(config);
+  
+ var database = firebase.database();
+
+ //grabs everything added & puts it on the page w/o refreshing
+ database.ref().on("child_added", function(childSnapshot, prevChildKey){
+  $("#recent").append( " <p> "  +  childSnapshot.val()  +  " </p> " );
+
+ 
+ });
+
+
   //on enter
   $("#usertext").keypress("keypress", function(e) {
     if (e.keyCode == 13) {
@@ -8,13 +30,13 @@ $(document).ready(function() {
       // "un-hide" the second div
       $("#youtubeDiv").removeAttr("style");
       //define variables equal to user input
-      var term = $(".form-control")
-        .val()
-        .trim();
-      var video = $(".form-control")
-        .val()
-        .trim();
+      var term = $(".form-control").val().trim();
+      var video = $(".form-control").val().trim();
       //targeting div youtube and adding html content
+
+    database.ref().push(term);
+
+    
 
       //ajax call
       var xhr = $.get("https://api.urbandictionary.com/v0/define?term=" + term);
@@ -35,9 +57,13 @@ $(document).ready(function() {
         $("#iframe").attr("src", "https://www.youtube.com/embed/" + videoid);
         //  close response function
       });
-    }
+  
+  }
   });
 
+  
+
+  
   //on click event
   $("#submit").click(function() {
     //clear out the div
@@ -45,6 +71,8 @@ $(document).ready(function() {
 
     // "un-hide" the second div
     $("#youtubeDiv").removeAttr("style");
+    $("#definition").removeAttr("style");
+
     //define variables equal to user input
     var term = $(".form-control")
       .val()
